d13d4f1945f33d8e354039c0a5659be716f25081
Sean Du <do4suki@gmail.com>
2026-04-18 07:21:48 +0800

    feat(json-to-go): add inline toggle for separated struct output

--- pages/json-to-go.tsx
+++ pages/json-to-go.tsx
@@ -1,13 +1,52 @@
 import ConversionPanel from "@components/ConversionPanel";
+import { EditorPanelProps } from "@components/EditorPanel";
+import Form, { InputType } from "@components/Form";
+import { useSettings } from "@hooks/useSettings";
 import { useCallback } from "react";
 import * as React from "react";
-import jsonToGo from "json-to-go";
+import jsonToGo from "@utils/jsonToGo";
 import gofmt from "gofmt.js";
 
+interface Settings {
+  inline: boolean;
+}
+
+const formFields = [
+  {
+    type: InputType.SWITCH,
+    key: "inline",
+    label: "Inline Nested Structs"
+  }
+];
+
 export default function JsonToGo() {
-  const transformer = useCallback(({ value }) => {
-    return gofmt(jsonToGo(value).go);
-  }, []);
+  const name = "JSON to Go Struct";
+  const [settings, setSettings] = useSettings(name, {
+    inline: true
+  });
+
+  const transformer = useCallback(
+    async ({ value }) => {
+      return gofmt(jsonToGo(value, undefined, { inline: settings.inline }).go);
+    },
+    [settings]
+  );
+
+  const getSettingsElement = useCallback<EditorPanelProps["settingElement"]>(
+    ({ open, toggle }) => {
+      return (
+        <Form<Settings>
+          title={name}
+          onSubmit={setSettings}
+          open={open}
+          toggle={toggle}
+          formsFields={formFields}
+          initialValues={settings}
+        />
+      );
+    },
+    []
+  );
 
   return (
     <ConversionPanel
@@ -16,6 +55,8 @@ export default function JsonToGo() {
       editorLanguage="json"
       resultTitle="Go"
       resultLanguage={"go"}
+      editorSettingsElement={getSettingsElement}
+      settings={settings}
     />
   );
 }
